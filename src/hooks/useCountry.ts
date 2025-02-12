605be2fefbaaacb103e455849346f4ebecc3bcab
Alexander Simoes <alexandersimoes@gmail.com>
2025-02-12 18:39:46 -0500

    reverts to manual country selection from csv

--- src/hooks/useCountry.ts
+++ src/hooks/useCountry.ts
@@ -1,14 +1,27 @@
+import { csv } from "d3-fetch";
 import { useEffect, useMemo, useState } from "react";
 import { countriesWithImage, Country } from "../domain/countries";
+
+interface DateCountry {
+  country: string;
+  date: string;
+}
 
 export function useCountry(dayString: string): [Country | undefined] {
   const [forcedCountryCode, setForcedCountryCode] = useState("");
 
   useEffect(() => {
-    const randomCountryCode =
-      countriesWithImage[Math.floor(Math.random() * countriesWithImage.length)]
-        .code;
-    setForcedCountryCode(randomCountryCode);
+    csv("/en/tradle/data.csv", (d) => {
+      return { country: d.country, date: d.date };
+    }).then((data) => {
+      setForcedCountryCode(
+        data.length
+          ? (
+              data.find((el) => el.date === dayString) as DateCountry
+            )?.country.toUpperCase() || ""
+          : ""
+      );
+    });
   }, [dayString]);
 
   const country = useMemo(() => {
