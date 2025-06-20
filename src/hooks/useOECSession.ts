1e372ba81df64ade0fae1c70eb86bed4e91e46e7
Pablo H. Paladino <paladinop@gmail.com>
2025-06-20 00:09:16 -0300

    fix oec session

--- src/hooks/useOECSession.ts
+++ src/hooks/useOECSession.ts
@@ -17,6 +17,7 @@ export type OECSession = SessionType | false | null;
   name: session.user.name,
   email: session.user.email,
   image: session.user.image,
+  history: []
 }
 */
 export function useOECSession() {
@@ -31,21 +32,45 @@ export function useOECSession() {
       "https://staging.oec.world",
     ];
 
-    targetOrigins.forEach((origin) => {
-      window.parent.postMessage("requestSession", origin);
-    });
+    let parentOrigin = "";
+    try {
+      // Try to get the parent origin from document.referrer
+      const referrerUrl = new URL(document.referrer);
+      parentOrigin = referrerUrl.origin;
+    } catch {
+      // fallback to '*'
+    }
+
+    // Only send to the matching origin in the list
+    const matchedOrigin = targetOrigins.find(
+      (origin) => origin === parentOrigin
+    );
+    if (parentOrigin !== "" && matchedOrigin) {
+      window.parent.postMessage(
+        {
+          type: "requestSession",
+          game: "pick-5",
+          history: true, // set to true to include user's history
+        },
+        matchedOrigin
+      );
+    } else {
+      console.warn("Parent origin not allowed");
+    }
 
     // Step 2: Handle response
     const handleMessage = (event: MessageEvent) => {
       // Allow localhost, oec.world subdomain or the root domain
       const url = new URL(event.origin);
       const { hostname } = url;
-      // const isLocalhost = hostname === "localhost";
-      const isLocalhost = false;
+      const isLocalhost = false; // change to true to test in local
       const isOecWorld =
         hostname.endsWith(".oec.world") || hostname === "oec.world";
 
-      if (!isLocalhost && !isOecWorld) return;
+      if (!isLocalhost && !isOecWorld) {
+        console.warn("Origin not allowed");
+        return;
+      }
 
       if (event.data?.type === "session") {
         setSession(event.data.session);
